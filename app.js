d4572eac41fc91c14e5a5fde3fecffe8ceab8ca3
alinamoony <itsalina.yb@gmail.com>
2025-03-07 18:00:19 +0700

    Testpush

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('hello world')
+const titleProject = "Оценка стоимости";
+const screensValue = "шаблонные, с уникальным дизайном, с анимациями";
+const screenPrice = 1500; 
+const percentage = 85;
+const fullPrice = 25000;
+const responsive = true;
+
+console.log(titleProject);
+console.log(screensValue);
+console.log(screenPrice);
+console.log(percentage);
+console.log(fullPrice);
+console.log(responsive);
+
+
+
