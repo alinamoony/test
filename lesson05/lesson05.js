db772fbb5e0894c518452b96618e50d57c078d1a
alinamoony <itsalina.yb@gmail.com>
2025-03-21 17:06:55 +0700

    lesson05

---
+++ lesson05/lesson05.js
@@ -0,0 +1,66 @@
+let titleProject = prompt("Название проекта?")
+console.log("Название проекта: ", titleProject)
+
+let screensValue = prompt("шаблонные, с уникальным дизайном, с анимациями")
+console.log("Нужно с чем: ", screensValue)
+
+let responsive = prompt("нужен ли респонсивный сайт?")
+console.log("Респонивность: ", responsive)
+
+let service1 = prompt("Какой сервис нужен?")
+console.log("Сервис: ", service1)
+
+let servicePrice1 = +prompt("Сколько это будет стоить?")
+console.log("Цена: ",servicePrice1)
+
+let service2 = prompt("Какой еще сервис тебе нужен?")
+console.log("Сервис: ", service2)
+
+let servicePrice2 = +prompt("Сколько будет стоить этот второй сервис?")
+console.log("Цена: ", servicePrice2)
+
+let screenPrice = 1500
+let fullPrice = screenPrice + servicePrice1 + servicePrice2
+console.log("Полная стоимость: ", fullPrice)
+
+let percentage = 10
+let servicePercentPrice = Math.round(fullPrice * (percentage / 100))
+console.log("Скидка: ", servicePercentPrice)
+
+
+let allServicePrices;
+
+let getTitle = function(word){
+    return word[0].toUpperCase() + word.slice(1).toLowerCase();
+}
+titleProject = getTitle(titleProject);
+console.log("Проект с заглавной буквы: ", titleProject);
+
+let getAllServicePrices = function() {
+    return servicePrice1 + servicePrice2;
+}
+allServicePrices = getAllServicePrices();
+console.log("Сумма всех дополнительных услуг: ", allServicePrices);
+
+function getFullPrice() {
+    return allServicePrices + screenPrice;
+}
+fullPrice = getFullPrice(); 
+
+let getServicePercentPrices = function(){
+    return fullPrice - servicePercentPrice;
+}
+servicePercentPrice = getAllServicePrices();
+
+function getRollbackMessage(){
+    if (fullPrice > 50000) {
+        console.log("Скидка 10%");
+    } else if (fullPrice > 20000 && fullPrice < 50000) {
+        console.log("Скидка 5%");
+    } else if (fullPrice < 20000 && fullPrice > 0) {
+        console.log("Нет скидки"); 
+    } else if (fullPrice == 0 || fullPrice == 20000 || fullPrice == 50000) {
+        console.log("ничего");
+    }
+}
+getRollbackMessage();
